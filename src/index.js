e34acec2d6c4475e9338f6fe64f75ffed73b7028
youzhihao <youzhihao233@163.com>
2019-09-11 11:44:07 +0800

    a

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './index.css';
 import App from './App';
+export default App
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
